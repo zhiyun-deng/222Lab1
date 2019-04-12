c20c5d10492be9b9d83c3ee78d5c3084c5007065
Zhiyun (David) Deng <43326016+zhiyun-deng@users.noreply.github.com>
2019-04-12 17:07:12 -0400

    Update g05_clock_divider.vhd

--- lab3/g05_clock_divider.vhd
+++ lab3/g05_clock_divider.vhd
@@ -1,18 +1,19 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+--this entity is designed to take in a 50MHz clock and output a 1Hz en-out signal
 entity g05_clock_divider is 
 	port(enable : in std_logic;
 			reset : in std_logic;
 			clk : in std_logic;
-			en_out: out std_logic := '0'
+			en_out: out std_logic := '0'--divided clock signal
 			);
 end entity;
 
 architecture a0 of g05_clock_divider is
-signal count : unsigned(25 downto 0) := to_unsigned(49999999, 26);
+signal count : unsigned(25 downto 0) := to_unsigned(49999999, 26);--50 million possible values
 begin
-
+--clock divider work by downcounting
 process(clk, reset)
 	begin
 		if(reset = '0') then
@@ -20,6 +21,7 @@ process(clk, reset)
 			en_out <= '0';
 		elsif(rising_edge(clk)) then
 			if(enable = '1') then 
+				--downcount
 				count <= count - 1;
 			else count<=count;
 			end if;
